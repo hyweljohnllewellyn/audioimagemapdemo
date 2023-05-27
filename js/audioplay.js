c941fe90c8915d0575c1fb649e52527a7951d20e
Hywel Llewellyn <hywel.llewellyn@gmail.com>
2023-05-27 14:49:30 +0100

    Update audioplay.js

--- js/audioplay.js
+++ js/audioplay.js
@@ -1,17 +1,19 @@
 $("map[name=my_image] area").on('click', function () {
-    var $this = $(this);
-     var id = $this.attr('id').replace(/area/, '');
-    
-    $.each($('audio'), function () {
-       this.pause();
-    });
-    
-        $this.toggleClass('active');
-        if($this.hasClass('active')){
-            $this.text('pause');
-          $('audio[id^="sound"]')[id-1].play();
-        } else {
-            $this.text('play');
-          $('audio[id^="sound"]')[id-1].pause();
-        }
-    });
+  var $this = $(this);
+   var id = $this.attr('id').replace(/area/, '');
+  
+  $.each($('audio'), function () {
+     this.pause();
+  });
+  
+      $this.toggleClass('active');
+      if($this.hasClass('active')){
+          // Removed text as requested by Don Wiss - 27 May 2023
+          //$this.text('pause');
+        $('audio[id^="sound"]')[id-1].play();
+      } else {
+          // Removed text as requested by Don Wiss - 27 May 2023
+          //$this.text('play');
+        $('audio[id^="sound"]')[id-1].pause();
+      }
+  });
